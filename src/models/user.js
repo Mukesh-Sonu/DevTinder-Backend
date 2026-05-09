d81b6173ee95d15ad0e974d3612001b10330fa28
Mukesh-Sonu <mukeshmuki761@gmail.com>
2026-05-09 09:51:32 +0530

    schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,61 @@
 const mongoose = require("mongoose");
+
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  emailId: String,
-  password: String,
-  age: Number,
-  gender: String,
-});
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true, //no error will be thrown, but it will conver to lowercase
+      trim: true, //no error will be thrown, but it will conver to trim
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        const genderSet = new Set(["male", "female", "others"]);
+        if (!genderSet.has(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://img.magnific.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?semt=ais_hybrid&w=740&q=80",
+    },
+    about: {
+      type: String,
+      default: "This is default about of the User",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const UserModel = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = UserModel;
+module.exports = User;
